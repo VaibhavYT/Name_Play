6e587dad8c53744fa64f930a72bcfa8b2f7420ac
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2022-12-22 12:28:26 +0530

    Complete

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//<button onclick="myFunction()">Try it</button>
+//document.getElementById('input-botton').onclick(myFunction());
+function myFunction() {
+    let person =document.getElementById("input-text").value;
+    let person2 = document.getElementById("input-text2").value;
+    if(person == null || person == ""){
+        alert('Please Enter Your Name');
+    }
+    else if(person=='Vaibhav' && person2=='Riya' || person=='vaibhav' && person2=='riya'){
+        window.alert('Ab itna bhi Khas nhi hu😁❤️');
+    }
+    else if(person2=='Riya' || person2=='riya'){
+        window.alert('Bhabhi Hai Tumhari 😁✌️');
+    }
+    else if(person=='Arjun' && person2 == 'Palakh' || person == 'arjun' && person2 == 'palakh' || person=='Arjun' && person2=='Palak' || person=='arjun' &&  person2=='palak'){
+        window.alert("Sallleeeh Nakli chapri berojgar");
+    }
+    
+    else{
+    //alert("I am an ${person} ");
+    window.alert(person + "Padhai Kro 😑");
+    }
+  }
+
